99436614cf41843b1089f2b599631fd9c190cfef
k-kazuma <k_kazuma8@icloud.com>
2023-05-18 07:17:14 +0900

    class

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,20 +1,33 @@
+import java.util.Scanner;
+
 public class HelloWorld {
 public static void main(String[] args) {
-    Test helloTest = new Test("kazuma", 27);
-    helloTest.hello();
-}
-}
-class Test {
-    String name;
-    int age;
+    Scanner setUserId = new Scanner(System.in);
+    Scanner setUserPass = new Scanner(System.in);
 
-    Test(String name, int age) {
-        name = this.name;
-        age = this.age;
+    try {
+        System.out.println("IDを入力してください:");
+        String userId = setUserId.nextLine();
+        System.out.println("パスワードを入力してください:");
+        String userPass = setUserPass.nextLine();
+        SineIn sineIn = new SineIn(userId, userPass);
+        sineIn.hello();
+    } finally {
+        setUserId.close();
+        setUserPass.close();
+    }
+}
+}
+class SineIn {
+    String id;
+    String pass;
+
+    SineIn(String id, String pass) {
+        this.id = id;
+        this.pass = pass;
     }
 
     public void hello() {
-        System.out.println("私の名前は" + name + "です");
-        System.out.println("年齢は" + age + "歳です");
+        System.out.println( id + "さんこんにちわ登録完了しました");
     }
 }
